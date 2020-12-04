f7b47a9acb0581d140c227f70c2ac9e4b75dadfc
DeanDal1000 <dalzelldean@gmail.com>
2020-12-04 12:22:08 +0000

    Fixing CHart API

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup'
 import styles from './Cards.module.css'
+import cx from 'classnames'
 
-//destructure API
+//destructure the data then API
 const  Cards = ({ data: { confirmed, recovered, deaths, lastUpdate} }) => {
     if(!confirmed) {
-        console.log(confirmed)
         return 'Loading...'
     }
     //Wrap in Container
     return ( <div className={styles.container}>
     <Grid container spacing={3} justify="center">
     {/* Card */}
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
             <CardContent>
                 <Typography color="textSecondary" gutterBottom>Infected</Typography>
                 <Typography variant="h5">{confirmed.value}</Typography>
-                <Typography color ="textSecondary">Real Date</Typography>
+                {/* counter */}
+                <CountUp start={0} end={confirmed.value} duration={2.5} separator=","/>
+                <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                 <Typography variant="body2">Number of active cases of Covid-19</Typography>
             </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
             <CardContent>
                 <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                <Typography variant="h5">Real Data</Typography>
-                <Typography color ="textSecondary">Real Date</Typography>
+                <Typography variant="h5">{recovered.value}</Typography>
+                <CountUp start={0} end={recovered.value} duration={2.5} separator=","/>
+                <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                 <Typography variant="body2">Number of recoveries of Covid-19</Typography>
             </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
             <CardContent>
                 <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                <Typography variant="h5">Real Data</Typography>
-                <Typography color ="textSecondary">Real Date</Typography>
+                <Typography variant="h5">{deaths.value}</Typography>
+                <CountUp start={0} end={deaths.value} duration={2.5} separator=","/>
+                <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                 <Typography variant="body2">Number of deaths of Covid-19</Typography>
             </CardContent>
         </Grid>
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { lineChart, barChart } from 'react-chartjs-2'
+import styles from './Chart.module.css'
 
 const  Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        //call API fucntion
+        fetchAPI()
+    });
+
+    const lineChart = (
+    dailyData[0]
+        ? (
+        <Line 
+            data={{
+                labels: '',
+                datasets:[{}, {}],
+            }}
+        />) : null
+        );
+
     return ( <h1>Chart</h1>  );
 }
  
